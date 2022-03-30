a722c6c31a74e6eff3ada051f247c4cc9fe8ffa7
Greenscreen23 <43916057+Greenscreen23@users.noreply.github.com>
2022-03-30 21:53:17 +0200

    Add warning when vscode git extension has not been enabled

--- src/gitApi.ts
+++ src/gitApi.ts
@@ -1,10 +1,16 @@
 import * as vscode from 'vscode';
+import { outputChannel } from './outputChannel';
 import { API, GitExtension } from './typings/git';
 
 let _gitApi: API | undefined;
 export function gitApi() {
+    const extension = vscode.extensions.getExtension<GitExtension>('vscode.git')?.exports;
+    if (!extension?.enabled) {
+        outputChannel().appendLine('Warning: The vscode git extension has not been enabled yet.');
+        return undefined;
+    }
     if (!_gitApi) {
-        _gitApi = vscode.extensions.getExtension<GitExtension>('vscode.git')?.exports.getAPI(1);
+        _gitApi = extension?.getAPI(1);
     }
     return _gitApi;
 }
--- src/typings/git.d.ts
+++ src/typings/git.d.ts
@@ -1,6 +1,7 @@
 /*---------------------------------------------------------------------------------------------
  *  Copyright (c) Microsoft Corporation. All rights reserved.
- *  Licensed under the MIT License. See License.txt on https://github.com/microsoft/vscode/ for license information.
+ *  Licensed under the MIT License. See License.txt on https://github.com/microsoft/vscode/
+ *  for license information.
  *--------------------------------------------------------------------------------------------*/
 
 import { Uri, Event, Disposable, ProviderResult } from 'vscode';
