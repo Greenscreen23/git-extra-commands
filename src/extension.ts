3d23cbda7dce4fee3c00888a3e637239940a00b0
Greenscreen23 <43916057+Greenscreen23@users.noreply.github.com>
2022-03-30 14:32:52 +0200

    Add modal before hard resetting

--- src/extension.ts
+++ src/extension.ts
@@ -1,13 +1,13 @@
 import * as vscode from 'vscode';
 import { exec } from 'child_process';
 
-import { outputChannel } from './singletons';
+import { outputChannel } from './outputChannel';
 import { getCWD, getCurrentBranch } from './helpers';
 
 export function activate(context: vscode.ExtensionContext) {
 
     context.subscriptions.push(vscode.commands.registerCommand('git-extra-commands.interactive-rebase', async () => {
-        const branch = await vscode.window.showInputBox({ placeHolder: 'Select a branch to rebase onto', title: 'Interactive rebase' } as vscode.InputBoxOptions);
+        const branch = await vscode.window.showInputBox({ placeHolder: 'Select a branch to rebase onto' } as vscode.InputBoxOptions);
         if(!branch) {
             outputChannel().appendLine('Warning: Recieved abort signal for "input rebase branch"');
             return;
@@ -21,7 +21,13 @@ export function activate(context: vscode.ExtensionContext) {
     }));
 
     context.subscriptions.push(vscode.commands.registerCommand('git-extra-commands.hard-reset', async () => {
-        const branch = await getCurrentBranch();
+        const ok = await vscode.window.showInformationMessage('You are about to overwrite your local changes with the state of origin.\n\nAre you sure to continue?', { modal: true }, 'Ok', 'Ok, Don\'t Ask Again');
+        if (!ok) {
+            outputChannel().appendLine('Warning: Recieved abort signal for "confirm hard reset"');
+            return;
+        }
+
+        const branch = getCurrentBranch();
         if (!branch) {
             outputChannel().appendLine('Error: Unable to read current branch');
             vscode.window.showInformationMessage('Extra Git Commands: Unable to read current branch');
